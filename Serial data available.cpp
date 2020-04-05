7520f82560a0f18d8e6858b66c07c457209bc76c
Nishan Chettri <35814272+nishanchettri@users.noreply.github.com>
2020-04-05 17:28:03 +0530

    Add files via upload

---
+++ Serial data available.cpp
@@ -0,0 +1,28 @@
+/*
+ * usart.cpp
+ *
+ * Created: 05-04-2020 14:51:59
+ *  Author: nishan chettri
+	Email: nishanchettri@gmail.com
+	Website: nishanchettri.com
+ */ 
+
+
+#include "uart.h"
+
+Serial ser;
+
+int main(void)
+{
+   
+   ser.begin(9600,12);//baud rate, crystal frequency
+
+   while(1)
+   {
+	   if(ser.available()>0)
+	   {
+		   ser.send("yes\n");
+	   }
+
+   }
+}
---
+++ SerialPrint Simple.cpp
@@ -0,0 +1,25 @@
+/*
+ * usart.cpp
+ *
+ * Created: 05-04-2020 14:51:59
+ *  Author: nishan chettri
+	Email: nishanchettri@gmail.com
+	Website: nishanchettri.com
+ */ 
+
+
+#include "uart.h"
+
+Serial ser;
+
+int main(void)
+{
+   
+   ser.begin(9600,12); //baud rate, crystal frequency
+   _delay_ms(500);
+   ser.send("hello world\n");
+   while(1)
+   {
+	   
+   }
+}
